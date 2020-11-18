a23084fd4c1c141485556e9de42b3490cfe12342
dsqsk <353333654@qq.com>
2020-11-18 11:24:06 +0800

    新增英雄关系字段

--- admin/src/views/Hero/HeroEdit.vue
+++ admin/src/views/Hero/HeroEdit.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>{{id ? '编辑':'新建'}}英雄</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      <el-tabs type="border-card" value="skills">
-        <el-tab-pane label="基本信息">
+      <el-tabs type="border-card" value="basic">
+        <el-tab-pane label="基本信息" name="basic">
           <el-form-item label='英雄名称'>
             <el-input v-model="model.name"></el-input>
           </el-form-item>
@@ -12,8 +12,16 @@
           </el-form-item>
           <el-form-item label='头像'>
             <el-upload class="avatar-uploader" :action="$http.defaults.baseURL + '/upload'" :show-file-list="false"
-              :on-success="afterUpload">
+              :on-success="afterUploadAvator">
               <img v-if="model.avatar" :src="model.avatar" class="avatar">
+              <i v-else class="el-icon-circle-plus-outline avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+
+          <el-form-item label='原画'>
+            <el-upload class="avatar-uploader" :action="$http.defaults.baseURL + '/upload'" :show-file-list="false"
+              :on-success="afterUploadBanner">
+              <img v-if="model.banner" :src="model.banner" class="avatar">
               <i v-else class="el-icon-circle-plus-outline avatar-uploader-icon"></i>
             </el-upload>
           </el-form-item>
@@ -74,11 +82,73 @@
                   <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                 </el-upload>
               </el-form-item>
+              <el-form-item label="冷却值">
+                <el-input v-model="item.cooldown"></el-input>
+              </el-form-item>
+              <el-form-item label="消耗">
+                <el-input v-model="item.consume"></el-input>
+              </el-form-item>
               <el-form-item label="技能描述">
                 <el-input type="textarea" v-model="item.description"></el-input>
               </el-form-item>
               <el-form-item>
                 <el-button size="small" type="danger" @click="model.skills.splice(index,1)">删除</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-tab-pane>
+        <!-- 搭档 -->
+        <el-tab-pane label="英雄关系">
+          <el-button icon="el-icon-plus" @click="model.partners.push({})">选择搭档英雄</el-button>
+          <el-row types="flex">
+            <!-- 通过el-col循环数据 -->
+            <el-col :md="12" v-for="(item,index) in model.partners" :key="index">
+              <el-form-item label="英雄" style="margin-top:2rem">
+                <el-select filterable v-model="item.hero">
+                  <el-option v-for="hero in heroes" :key="hero._id" :value="hero._id" :label="hero.name"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="描述">
+                <el-input type="textarea" v-model="item.description"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" type="danger" @click="model.partners.splice(index,1)">删除</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 被克 -->
+          <el-button icon="el-icon-plus" style="margin-top:2rem" @click="model.counters.push({})">选择克制英雄</el-button>
+          <el-row types="flex">
+            <!-- 通过el-col循环数据 -->
+            <el-col :md="12" v-for="(item,index) in model.counters" :key="index">
+              <el-form-item label="英雄" style="margin-top:2rem">
+                <el-select filterable v-model="item.hero">
+                  <el-option v-for="hero in heroes" :key="hero._id" :value="hero._id" :label="hero.name"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="描述">
+                <el-input type="textarea" v-model="item.description"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" type="danger" @click="model.counters.splice(index,1)">删除</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 克制 -->
+          <el-button icon="el-icon-plus" style="margin-top:2rem" @click="model.advantagers.push({})">选择克制英雄</el-button>
+          <el-row types="flex">
+            <!-- 通过el-col循环数据 -->
+            <el-col :md="12" v-for="(item,index) in model.advantagers" :key="index">
+              <el-form-item label="英雄" style="margin-top:2rem">
+                <el-select filterable v-model="item.hero">
+                  <el-option v-for="hero in heroes" :key="hero._id" :value="hero._id" :label="hero.name"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="描述">
+                <el-input type="textarea" v-model="item.description"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" type="danger" @click="model.advantagers.splice(index,1)">删除</el-button>
               </el-form-item>
             </el-col>
           </el-row>
@@ -101,16 +171,20 @@ export default {
       model: {
         name: '',
         avatar: '',
+        skills: [],
+        partners: [],
+        counter: [],
+        advantagers: [],
         scores: {
           difficultGrade: 0,
           skillGrade: 0,
           attackGrade: 0,
           survivalGrade: 0
-        },
-        skills: []
+        }
       },
       categories: [],
-      items: []
+      items: [],
+      heroes: []
     }
   },
   methods: {
@@ -122,7 +196,7 @@ export default {
       } else {
         res = await this.$http.post('/rest/heroes', this.model)
       }
-      this.$router.push('/heroes/list')
+      // this.$router.push('/heroes/list')
       this.$message({
         type: 'success',
         message: '保存成功'
@@ -140,13 +214,21 @@ export default {
       const res = await this.$http.get('/rest/items')
       this.items = res.data
     },
-    afterUpload (res) {
+    async fetchHeroes () {
+      const res = await this.$http.get('/rest/heroes')
+      this.heroes = res.data
+    },
+    afterUploadAvator (res) {
       this.model.avatar = res.url
+    },
+    afterUploadBanner (res) {
+      this.model.banner = res.url
     }
   },
   created () {
     this.fetchItems()
     this.fetchCategories()
+    this.fetchHeroes()
     this.id && this.fetchList()
   }
 }
--- admin/src/views/Layout.vue
+++ admin/src/views/Layout.vue
@@ -62,7 +62,6 @@
             <el-dropdown-item>删除</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-        <span>王小虎</span>
       </el-header>
       <el-main>
         <router-view :key="$route.path"></router-view>
